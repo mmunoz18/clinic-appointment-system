717ad1b67fb5867ef391168ce2f9e9852eff698f
mmunoz18 <43831255+mmunoz18@users.noreply.github.com>
2026-06-10 14:41:41 -0600

    Display dashboard

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,5 +1,26 @@
 function DashboardPage() {
-  return <h1>Dashboard</h1>;
+  return (
+    <section>
+      <h1>Dashboard</h1>
+
+      <div>
+        <div>
+          <h3>Doctors</h3>
+          <p>View and manage doctors</p>
+        </div>
+
+        <div>
+          <h3>Patients</h3>
+          <p>View and manage patients</p>
+        </div>
+
+        <div>
+          <h3>Appointments</h3>
+          <p>Schedule and manage appointments</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default DashboardPage;
